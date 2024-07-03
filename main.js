586c28d7c07c7c2d11b16f2f61722ecbd53c453e
bojana-petroska <bojana.petroska@yahoo.com>
2024-07-03 17:40:15 +0200

    3

--- main.js
+++ main.js
@@ -25,3 +25,21 @@ const bubbleSort = (arr) => {
   
     return merge(left, right);
   };
+
+  const merge = (left, right) => {
+    const result = [];
+    let i = 0;
+    let j = 0;
+
+    while(i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    return result.concat(left.slice(i).concat(right.slice(j)));
+}
