abc72474ba78bb4e7968ccf5f30d082bffe79a8f
bojana-petroska <bojana.petroska@yahoo.com>
2024-07-03 19:07:26 +0200

    start

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-console.log('bo');
+const csv = require('csv-parser');
+const fs = require('fs');
+const results = [];
+
+fs.createReadStream('items.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    //console.log(results); // You can now work with the parsed CSV data in the `results` array
+
+    const filteredResults = results.filter(item => item.category === 'Tops');
+    console.log(filteredResults.length);
+
+    results.forEach(item => {
+        console.log(item.name);
+    })
+});
+
+// View Available Items: Users can browse through the inventory of clothing items available in the store.
 
