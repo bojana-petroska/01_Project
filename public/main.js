e0f6c5f4653ca8e2874acd8d464daff912db6311
bojana-petroska <bojana.petroska@yahoo.com>
2024-07-04 12:45:46 +0200

    first half day

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
 // const csv = require('csv-parser');
 // const fs = require('fs');
+// import csv from 'csv-parser';
+// import fs from 'fs';
+
 const results = [];
+console.log('bo');
 
 // fs.createReadStream('items.csv')
 //   .pipe(csv())
@@ -9,50 +13,65 @@ const results = [];
 //     results.push(data);
 //   })
 //   .on('end', () => {
-//     //console.log(results); // You can now work with the parsed CSV data in the `results` array
+//     //console.log(results);
+//     // You can now work with the parsed CSV data in the `results` array
 
-//     const filteredResults = results.filter(item => item.category === 'Tops');
-//     //console.log(filteredResults.length);
+//     const filteredResults = results.filter((item) => item.category === 'Tops');
+//     //console.log(filteredResults);
 
-//     results.forEach(item => {
-//         console.log(`Item: ${item.name}`);
-//         console.log(`Image: ${item.image}`);
-//        // console.log(item.price);
-//         //console.log(item.image);
-//     });
+//     // results.forEach(item => {
+//     //     console.log(`Item: ${item.name}`);
+//     //     console.log(`Image: ${item.image}`);
+//     //     console.log(item.price);
+//     //     console.log(item.image);
+//     // });
+
+//     function displayItems(items) {
+//       const itemTemplate = document.getElementById('item-layout');
+//       const displayedItems = document.getElementById('item');
+
+//       items.forEach((item) => {
+//         const clone = document.importNode(itemTemplate.content, true);
+//         clone.querySelector('.item-image').src = item.image;
+//         clone.querySelector('.item-name').textContent = item.name;
+//         clone.querySelector('.item-price').textContent = item.price;
+
+//         displayedItems.appendChild(clone);
+//       });
+//     }
+
+//     console.log(results);
+//     displayItems(results);
+//   });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('DOMContentLoaded event fired');
+
+//   // Fetch data
+//   fetch('/items')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(`Data: ${data}`);
+
+// //View Available Items: Users can browse through the inventory of clothing items available in the store.
+
+//       const displayItems = document.getElementById('view-available-items');
+//       const itemLayout = document.getElementById('item-layout').content;
+
+//       data.forEach((item) => {
+//         const clone = itemLayout.cloneNode(true);
+//         clone.querySelector('.item-name').textContent = item.name;
+//         clone.querySelector('.item-image').src = item.image;
+//         clone.querySelector('.item-image').alt = item.name;
+//         clone.querySelector(
+//           '.item-price'
+//         ).textContent = `Price: $${item.price}`;
+
+//         // 'Add to Cart' button functionality comes here
+//         displayItems.appendChild(clone);
+//       });
+//     })
+//     .catch((error) => console.error('Error fetching items:', error));
 // });
 
-// View Available Items: Users can browse through the inventory of clothing items available in the store.
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  console.log('DOMContentLoaded event fired');
-
-  // Fetch data
-  fetch('/items')
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Data: ${data}`);
-
-      const displayItems = document.getElementById('view-available-items');
-      const itemLayout = document.getElementById('item-layout').content;
-
-      data.forEach(item => {
-        const clone = itemLayout.cloneNode(true);
-        console.log(`Item: ${item}`);
-
-        clone.querySelector('.item-name').textContent = item.name;
-        clone.querySelector('.item-image').src = item.image;
-        clone.querySelector('.item-image').alt = item.name;
-        clone.querySelector('.item-price').textContent = `Price: $${item.price}`;
-        clone.querySelector('.item-color').textContent = `Color: ${item.color}`;
-        clone.querySelector('.item-size').textContent = `Size: ${item.size}`;
-
-        displayItems.appendChild(clone);
-      });
-    })
-    .catch(error => console.error('Error fetching items:', error));
-});
-
-console.log('bo')
 
