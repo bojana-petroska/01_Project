36f6130a2150667872177acf76dc8ed77d7a8dc8
bojana-petroska <bojana.petroska@yahoo.com>
2024-07-04 16:41:51 +0200

    search logic

--- public/main.js
+++ public/main.js
@@ -2,20 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
   loadCSV('items.csv', (data) => {
     const inventory = parseCSV(data);
     console.log(inventory);
-    displayItems(inventory);  
+    displayItems(inventory);
+
+    const searchForm = document.getElementById('search-form');
+
+    searchForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const searchNameInput = document
+        .getElementById('search-name')
+        .value.trim()
+        .toLowerCase();
+      const searchSizeInput = document
+        .getElementById('search-size')
+        .value.trim();
+
+      const results = searchItems(inventory, searchNameInput, searchSizeInput);
+
+      displayItems(results);
+    });
   });
 });
 
 function loadCSV(url, callback) {
   fetch(url)
-    .then(response => response.text())
-    .then(data => callback(data))
-    .catch(error => console.error('Error loading CSV file:', error));
+    .then((response) => response.text())
+    .then((data) => callback(data))
+    .catch((error) => console.error('Error loading CSV file:', error));
 }
 
 function parseCSV(data) {
   const lines = data.split('\n');
-  const headers = lines[0].split(',').map(header => header.trim());
+  const headers = lines[0].split(',').map((header) => header.trim());
   const items = [];
 
   for (let i = 1; i < lines.length; i++) {
@@ -33,8 +51,8 @@ function parseCSV(data) {
 
 function displayItems(items) {
   const itemTemplate = document.getElementById('item-layout');
-  const displayedItems = document.getElementById('view-available-items');  
-  displayedItems.innerHTML = ''; 
+  const displayedItems = document.getElementById('view-available-items');
+  displayedItems.innerHTML = '';
 
   items.forEach((item) => {
     const clone = document.importNode(itemTemplate.content, true);
@@ -46,5 +64,14 @@ function displayItems(items) {
   });
 }
 
+const searchItems = (items, searchName = '', searchSize = '') => {
+  // Filter items based on search criteria
+  const results = items.filter((item) => {
+    const nameMatch = item.name.toLowerCase().includes(searchName);
+    const sizeMatch = item.size.toLowerCase() === searchSize.toLowerCase();
+    return (searchName === '' || nameMatch) && (searchSize === '' || sizeMatch);
+  });
 
+  return results;
+};
 
